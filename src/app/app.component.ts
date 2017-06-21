6a7171c64c1baae7d220771f5ba1873fa170bf67
Ravi Ranjan <ravi.ranjan@tudip.nl>
2017-06-21 11:18:10 +0530

    Modified components on heros.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   	title = 'Tour of Heroes';
-  	hero = 'Sumit';
-    myVar = 'adhkgjhkjshdfgkjhdkfsdgkfjgas';
-
+  	hero: Hero={id: 1, name: 'Windstorm'};
+}
+export class Hero {
+  id: number;
+  name: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
@@ -10,7 +11,8 @@ import { ProductsComponent } from './products/products.component';
     ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
